7d24669717151def88a9fea85e5e759984f95e7a
nada <nada.nasr436@gmail.com>
2025-05-14 02:40:52 +0300

    new commit

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-import express from 'express'
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors'; // ✅ الصح هنا
 dotenv.config();
+
 import { bootstrap } from './src/app.controller.js';
-const app = express()
-const port = process.env.port 
-bootstrap(app,express)
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
+const app = express();
+const port = process.env.port;
 
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
 
+// Middleware
+app.use(express.json());
 
+// Bootstrapping your routes or setup
+bootstrap(app, express);
+
+app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
