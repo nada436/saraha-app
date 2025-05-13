c77bd8aadd8bc4a8b44d3029a53689698f2c6f13
nada <nada.nasr436@gmail.com>
2025-05-14 00:06:28 +0300

    new commit

--- src/modules/ussers/users.servece.js
+++ src/modules/ussers/users.servece.js
@@ -81,7 +81,7 @@ export const my_profile=async(req,res) => {
     const mymessages=await messagemodel.find({user_id:req.user._id})
      const host = req.get('host'); 
      const protocol = req.protocol;
-     res.json({...req.user,phone,mymessages,my_profile_url:`${protocol}://${host}/messages/send/${req.user._id}`})
+     res.json({...req.user,phone,mymessages,my_profile_url:`${protocol}://${host}/users/share/${req.user._id}`})
 }
 
 
